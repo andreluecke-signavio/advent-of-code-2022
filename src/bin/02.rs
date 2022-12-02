fa8115849f7c8f6f06a12e53b3b7a952619a700d
Andre Luecke <andre.luecke@signavio.com>
2022-12-02 09:34:53 +0100

    day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,65 @@
+pub fn part_one(input: &str) -> Option<u32> {
+
+    let result = input.lines().map(|s| {
+        match s {
+            "A X" => 1 + 3,
+            "A Y" => 2 + 6,
+            "A Z" => 3 + 0,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 1 + 6,
+            "C Y" => 2 + 0,
+            "C Z" => 3 + 3,
+            _ => 0
+        }
+    })
+        .sum();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+
+    let result = input.lines().map(|s| {
+        match s {
+            "A X" => 3 + 0,
+            "A Y" => 1 + 3,
+            "A Z" => 2 + 6,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 2 + 0,
+            "C Y" => 3 + 3,
+            "C Z" => 1 + 6,
+            _ => 0
+        }
+    })
+        .sum();
+
+    Some(result)
+
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 2);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_one(&input), Some(15));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_two(&input), Some(12));
+    }
+}
